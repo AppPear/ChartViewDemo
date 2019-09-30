dabd2686da0df883407357b21c37718d0f317532
Andras Samu <samu.andris1@gmail.com>
2019-09-30 10:22:35 +0200

    Initial Commit

---
+++ TestChartViews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestChartViews
+//
+//  Created by András Samu on 2019. 09. 30..
+//  Copyright © 2019. András Samu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestChartViewsTests/TestChartViewsTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestChartViewsTests.swift
+//  TestChartViewsTests
+//
+//  Created by András Samu on 2019. 09. 30..
+//  Copyright © 2019. András Samu. All rights reserved.
+//
+
+import XCTest
+@testable import TestChartViews
+
+class TestChartViewsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
